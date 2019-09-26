11cd6b8c437b4227bb4bb00fab8d6e896ef53634
jaboud <jaboud94@gmail.com>
2019-09-26 18:58:43 +1000

    Created conditional statement to to see checks whether if the 'conversionSuccess' boolean is true then returns the time.

--- HackerRank-Time-Conversion/Program.cs
+++ HackerRank-Time-Conversion/Program.cs
@@ -10,6 +10,13 @@ namespace HackerRank_Time_Conversion
             //Boolean variable to see if our converison was sucessful from 12 hour to 24 hour while calling the DateTime class in C# using the TryParse method which it
             // will take the input string and the out variable of the DateTime class.
             bool conversionSuccess = DateTime.TryParse(s, out DateTime time);
+
+            //Checks whether if the 'conversionSuccess' boolean is true
+            if (conversionSuccess)
+            {
+                //If true then it will grab our out variable of 'time' convert it to a string.
+                return time.ToString("hh:mm:ss");
+            }
       
         }
         static void Main(string[] args)

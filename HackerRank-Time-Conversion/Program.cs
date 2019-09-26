6a6483d699ea1017234eb6899ca25f6219b4691e
jaboud <jaboud94@gmail.com>
2019-09-26 18:00:31 +1000

    Created Initial project solution.

---
+++ HackerRank-Time-Conversion/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HackerRank_Time_Conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
